530961a0557906b867c7384cee1546e48120c633
HHHJiro <1445422321@qq.com>
2017-03-10 15:20:10 +0800

    整理了文件目录 新增两个html

--- js/index.js
+++ js/index.js
@@ -8,49 +8,72 @@ var vm = new Vue({
 				size:4,
 				content:[{
 					ctitle:'腾讯首页',
-					href:'pages/cssabout/qq_index/html/qq.html'
+					href:'pages/chapters1/qq_index/html/qq.html'
 					},
 					{
 					ctitle:'照片墙',
-					href:'pages/cssabout/album/album.html'
+					href:'pages/chapters1/album/album.html'
 					},
 					{
 						ctitle:'div属性改变',
-						href:'pages/cssabout/change-div.html'
+						href:'pages/chapters1/change-div.html'
 					},
 					{
-						ctitle:'字符串反转',
-						href:'pages/cssabout/reverse.html'
+						ctitle:'css 三角形',
+						href:'pages/chapters1/cssTriangle.html'
 					},
-					{
-						ctitle:'上传图片预览',
-						href:'pages/cssabout/preview.html'
-					}
 					]
 			},
 			{
 				title:'Chapter 2',
 				size:4,
 				show:true,
+				content:[
+					{
+						ctitle:'字符串反转',
+						href:'pages/chapters2/reverse.html'
+					},
+					{
+						ctitle:'字符串数字求和',
+						href:'pages/chapters2/numcount.html'
+					},
+					{
+						ctitle:'字符串去重',
+						href:'pages/chapters2/removeSame.html'
+					},
+					{
+						ctitle:'字符串截取',
+						href:'pages/chapters2/stringsub.html'
+					},
+					]
+			},
+			{
+				title:'Chapter 3',
+				size:4,
+				show:true,
 				content:[{
 					ctitle:'js轮播图',
-					href:'pages/jsabout/PicCarousel/PicCarousel.html'
+					href:'pages/chapters3/PicCarousel/PicCarousel.html'
+					},
+					{
+						ctitle:'上传图片预览',
+						href:'pages/chapters3/preview.html'
 					},
 					{
 					ctitle:'js滚动图',
-					href:'pages/jsabout/heroroll/framcc.html'
+					href:'pages/chapters3/heroroll/framcc.html'
 					},
 					{
 					ctitle:'js选项卡',
-					href:'pages/jsabout/tab/tab2.html'
+					href:'pages/chapters3/tab/tab2.html'
 					},
 					{
 					ctitle:'js日历',
-					href:'pages/jsabout/calendar/calendar_nooutjs.html'
+					href:'pages/chapters3/calendar/calendar_nooutjs.html'
 					},
 					{
 					ctitle:'原生js select插件',
-					href:'pages/jsabout/select/index.html'
+					href:'pages/chapters3/select/index.html'
 					}],
 			}
 			],
